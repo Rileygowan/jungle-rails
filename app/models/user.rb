2dd9156bb0b9a425a3c38473f2dbdee544f49bfd
Rileygowan <t.rileygowan@gmail.com>
2017-11-13 21:12:46 +0000

    added user tests and updated sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
+  validates :password, presence: true, :length => {:within => 2..10}
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   has_many :orders
   has_many :reviews
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.delete(' '))
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should be valid when the passwords match' do
+      @user = User.new(
+          first_name: 'riley',
+          last_name: 'gowan',
+          email: 't.rileygowan@gmail.com',
+          password: 'hello',
+          password_confirmation: 'hello'
+        )
+      expect(@user).to be_valid
+    end
+    it 'should not be valid when the passwords do not match' do
+      @user = User.new(
+          first_name: 'riley',
+          last_name: 'gowan',
+          email: 't.rileygowan@gmail.com',
+          password: 'hello',
+          password_confirmation: 'hey'
+        )
+      expect(@user).to_not be_valid
+    end
+    it 'should not be valid when a password is not provided' do
+      @user = User.new(
+          first_name: 'riley',
+          last_name: 'gowan',
+          email: 't.rileygowan@gmail.com',
+          password: "hey",
+          password_confirmation: nil
+        )
+      expect(@user).to_not be_valid
+    end
+    it 'should not be valid when an given email already exists in the database' do
+      @user1 = User.new(
+          first_name: 'riley',
+          last_name: 'gowan',
+          email: 't.rileygowan@gmail.com',
+          password: "hey",
+          password_confirmation: 'hey'
+        )
+      @user1.save!
+
+      @user2 = User.new(
+          first_name: 'jim',
+          last_name: 'guthrie',
+          email: 't.RILEYgowan@gmail.com',
+          password: "hey",
+          password_confirmation: 'hey'
+        )
+      expect(@user2).to_not be_valid
+    end
+    it 'should not be valid if the given password is either too long or too short' do
+      @user = User.new(
+          first_name: 'rick',
+          last_name: 'roll',
+          email: 'rickroll@hotmail.com',
+          password: 'a',
+          password_confirmation: 'a'
+        )
+      expect(@user).to_not be_valid
+    end
+    it 'should log in a user regardless of whitespace' do
+      @user = User.new(
+          first_name: 'rick',
+          last_name: 'roll',
+          email: '  rickroll@hotmail.com',
+          password: 'hello',
+          password_confirmation: 'hello'
+        )
+      expect(@user).to be_valid
+    end
+  end
+  describe '.authenticate_with_credentials' do
+    it 'should create a new session if the user exists in the database and password is valid' do
+      @user = User.new(
+          first_name: 'riley',
+          last_name: 'gowan',
+          email: 'rileygowan@hotmail.com',
+          password: 'hello',
+          password_confirmation: 'hello'
+        )
+      @user.save!
+      user = User.authenticate_with_credentials(@user.email, @user.password)
+      expect(user).to be_truthy
+    end
+  end
 end
