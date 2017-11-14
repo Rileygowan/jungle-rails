136db2e55776fbba705424cf62e57ea8dc763aaa
Rileygowan <t.rileygowan@gmail.com>
2017-11-14 18:56:11 +0000

    test to check UX navigation to product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to product details", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can navigate to product details from homepage" do
+    visit root_path
+    link = page.find_link('Details', match: :first)
+    link.click
+
+    expect(page).to have_css 'article.product-detail'
+    save_screenshot
+  end
+
 end
